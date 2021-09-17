9d19976667b50452f7abc6bab1b38a0757b07cc7
ashndev <87834445+ashndev@users.noreply.github.com>
2021-09-17 13:13:03 -0700

    AppRouter initialization fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bottom_navigation_with_nested_routing_tutorial/routes/router.gr.dart';
 
-void main() => runApp(const AppWidget());
+void main() => runApp(AppWidget());
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
-
+  AppWidget({Key? key}) : super(key: key);
+  final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    final _appRouter = AppRouter();
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Bottom Nav Bar with Nested Routing',
